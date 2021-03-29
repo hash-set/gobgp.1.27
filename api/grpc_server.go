ab518d35cb22b80bf97d6aeb1f443de5e9c119b8
Kunihiro Ishiguro <ishi@hash-set.com>
2021-03-29 12:51:35 -0700

    Revert format change.

--- api/grpc_server.go
+++ api/grpc_server.go
@@ -907,8 +907,8 @@ func (s *Server) AddBmp(ctx context.Context, arg *AddBmpRequest) (*AddBmpRespons
 		return nil, fmt.Errorf("invalid bmp route monitoring policy: %d", arg.Type)
 	}
 	return &AddBmpResponse{}, s.bgpServer.AddBmp(&config.BmpServerConfig{
-		Address:               arg.Address,
-		Port:                  arg.Port,
+		Address: arg.Address,
+		Port:    arg.Port,
 		RouteMonitoringPolicy: t,
 	})
 }
@@ -1020,7 +1020,7 @@ func (s *Server) EnableZebra(ctx context.Context, arg *EnableZebraRequest) (*Ena
 		}
 	}
 	return &EnableZebraResponse{}, s.bgpServer.StartZebraClient(&config.ZebraConfig{
-		Url:                       arg.Url,
+		Url: arg.Url,
 		RedistributeRouteTypeList: l,
 		Version:                   uint8(arg.Version),
 		NexthopTriggerEnable:      arg.NexthopTriggerEnable,
